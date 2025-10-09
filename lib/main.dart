17511e5d98413018e61d85676d9b988d781da7c0
PhuCG <nguyentrongphu143@gmail.com>
2025-10-09 15:46:50 +0700

    Initialize StorageService in main.dart to ensure data storage is ready before app launch.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_foreground_task/flutter_foreground_task.dart';
+import 'package:test_abc/services/storage_service.dart';
 import 'screens/home_screen.dart';
 import 'services/step_counter_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await StorageService.instance.initialize();
   FlutterForegroundTask.initCommunicationPort();
   // Initialize step counter service
   await StepCounterService.instance.initialize();
